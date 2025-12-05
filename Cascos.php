59250e021e93386b2aaf765ded92e1a31e1c059b
Victor <oopv080329hgtrrca5@cbtis217.edu.mx>
2025-12-05 08:25:51 -0600

    v1. Base de archivos

---
+++ Cascos.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <!--Vista Tipos de Cascos (se alimenta del CRUD de Cascos)-->
+</head>
+<body>
+    
+</body>
+</html>
---
+++ FAQ.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <!--Vista Preguntas Frecuentes (FAQ) (se alimenta del CRUD de Accidentes)-->
+</head>
+<body>
+    
+</body>
+</html>
